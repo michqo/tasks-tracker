916d16121604f952c50ede1b28e53d08d2964acb
michqo <42279279+michqo@users.noreply.github.com>
2024-04-12 10:32:07 +0200

    feat: change styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,5 @@
 <script>
-	import '../app.pcss';
+  import '../app.pcss';
 </script>
 
 <slot />
-
-<style lang="postcss">
-	:global(body) {
-		@apply text-indigo-100 bg-slate-900 mx-3;
-	}
-</style>
