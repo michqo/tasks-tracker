bcd924d64f3af104d3cf184d49ac6eed3ba1cc11
michqo <42279279+michqo@users.noreply.github.com>
2024-04-12 12:53:40 +0200

    feat: change tailwind theme for shadcn

--- src/lib/components/Todo.svelte
+++ src/lib/components/Todo.svelte
@@ -34,14 +34,14 @@
 <div class="p-4">
   <h1 class="mb-4 text-2xl font-bold">Todo List</h1>
 
-  {#each todos as todo (todo.id)}
+  {#each todos as todo, idx (todo.id)}
     <div
       class="flex items-center justify-between border-b border-gray-300 py-2 {todo.completed
         ? 'line-through'
         : ''}"
     >
       <span>{todo.title}</span>
-      <input type="checkbox" checked={todo.completed} on:change={() => toggleCompletion(todo.id)} />
+      <input type="checkbox" checked={todo.completed} on:change={() => toggleCompletion(idx)} />
     </div>
   {/each}
 </div>
