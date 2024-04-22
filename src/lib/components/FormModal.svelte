5b4195d8c58d4be9f173e136eec1eb333754720f
michqo <42279279+michqo@users.noreply.github.com>
2024-04-22 11:13:05 +0200

    feat: example todo modal

---
+++ src/lib/components/FormModal.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { Button } from '$lib/components/ui/button/index.js';
+  import * as Card from '$lib/components/ui/card/index.js';
+  import { Input } from '$lib/components/ui/input/index.js';
+  import { Label } from '$lib/components/ui/label/index.js';
+</script>
+
+<Card.Root class="w-[350px]">
+  <Card.Header>
+    <Card.Title>Create todo</Card.Title>
+    <Card.Description>Write your ideas here</Card.Description>
+  </Card.Header>
+  <Card.Content>
+    <form>
+      <div class="grid w-full items-center gap-4">
+        <div class="flex flex-col space-y-1.5">
+          <Label for="title">Title</Label>
+          <Input id="title" placeholder="Title" />
+        </div>
+      </div>
+    </form>
+  </Card.Content>
+  <Card.Footer class="flex justify-between">
+    <Button variant="outline">Cancel</Button>
+    <Button>Add</Button>
+  </Card.Footer>
+</Card.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import FormModal from '$lib/components/FormModal.svelte';
   import Todo from '$lib/components/Todo.svelte';
 </script>
 
+<FormModal />
 <Todo />
