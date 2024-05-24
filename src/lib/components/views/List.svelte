a1ec717dfd48b562344dcaf8a3abf066849c2d71
michqo <42279279+michqo@users.noreply.github.com>
2024-05-24 12:01:41 +0200

    feat: list view drag and drop

--- src/lib/components/views/List.svelte
+++ src/lib/components/views/List.svelte
@@ -1,32 +1,53 @@
 <script lang="ts">
   import { Checkbox } from '$lib/components/ui/checkbox';
-  import { dropdownOpen, todos } from '$lib/utils/stores';
+  import { cn } from '$lib/utils';
+  import { todos } from '$lib/utils/stores';
+  import type { Todo } from '$lib/utils/types';
   import { GripVertical } from 'lucide-svelte';
+  import { dragHandle, dragHandleZone, type DndEvent } from 'svelte-dnd-action';
+  import { flip } from 'svelte/animate';
   import TodoContextMenu from '../TodoContextMenu.svelte';
-  import Button from '../ui/button/button.svelte';
+  import { buttonVariants } from '../ui/button';
+
+  const flipDurationMs = 300;
 
   function onDelete(idx: number) {
     $todos.splice(idx, 1);
     $todos = $todos;
   }
+  function handleSort(e: CustomEvent<DndEvent<Todo>>) {
+    $todos = e.detail.items;
+  }
 </script>
 
-{#each $todos as todo, idx (todo.id)}
-  <TodoContextMenu bind:checked={todo.completed} on:delete={() => onDelete(idx)}>
-    <div
-      class="flex items-center justify-between border-b border-gray-300 py-2 {todo.completed
-        ? 'line-through'
-        : ''}"
-    >
-      <div class="flex items-center gap-x-3">
-        <Checkbox bind:checked={todo.completed} />
-        <span>{todo.title}</span>
-      </div>
-      <div class="flex items-center gap-x-2">
-        <Button on:click={() => ($dropdownOpen = true)} variant="outline" size="icon">
-          <GripVertical size={20} />
-        </Button>
-      </div>
+<section
+  use:dragHandleZone={{ items: $todos, flipDurationMs, dropTargetStyle: {} }}
+  on:consider={handleSort}
+  on:finalize={handleSort}
+>
+  {#each $todos as todo, idx (todo.id)}
+    <div animate:flip={{ duration: flipDurationMs }}>
+      <TodoContextMenu bind:checked={todo.completed} on:delete={() => onDelete(idx)}>
+        <div
+          class="flex items-center justify-between border-b border-gray-300 py-2 {todo.completed
+            ? 'line-through'
+            : ''}"
+        >
+          <div class="flex items-center gap-x-3">
+            <Checkbox bind:checked={todo.completed} />
+            <span>{todo.title}</span>
+          </div>
+          <div class="flex items-center gap-x-2">
+            <div
+              use:dragHandle
+              aria-label="drag-handle for {todo.id}"
+              class={cn(buttonVariants({ variant: 'outline', size: 'icon', className: 'handle' }))}
+            >
+              <GripVertical size={20} />
+            </div>
+          </div>
+        </div>
+      </TodoContextMenu>
     </div>
-  </TodoContextMenu>
-{/each}
+  {/each}
+</section>
