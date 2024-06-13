d303a788d8e2a6458567b1203a894547d2097413
michqo <42279279+michqo@users.noreply.github.com>
2024-06-13 22:34:36 +0200

    feat: auth tabs

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import * as Card from '$lib/components/ui/card/index.js';
+  import * as Card from '$lib/components/ui/card';
   import * as Form from '$lib/components/ui/form';
-  import { Input } from '$lib/components/ui/input/index.js';
+  import { Input } from '$lib/components/ui/input';
+  import * as Tabs from '$lib/components/ui/tabs';
   import { api } from '$lib/utils/api';
   import { loginSchema } from '$lib/utils/schemas';
   import { token } from '$lib/utils/stores';
@@ -27,31 +28,42 @@
 </script>
 
 <main class="grid h-screen items-center justify-center">
-  <Card.Root class="w-[350px]">
-    <Card.Header>
-      <Card.Title>Log in to tasks tracker</Card.Title>
-      <Card.Description>Enter your credentials.</Card.Description>
-    </Card.Header>
-    <form method="POST" use:enhance>
-      <Card.Content>
-        <Form.Field {form} name="username">
-          <Form.Control let:attrs>
-            <Form.Label>Username</Form.Label>
-            <Input {...attrs} bind:value={$formData.username} />
-          </Form.Control>
-          <Form.FieldErrors />
-        </Form.Field>
-        <Form.Field {form} name="password">
-          <Form.Control let:attrs>
-            <Form.Label>Password</Form.Label>
-            <Input {...attrs} bind:value={$formData.password} />
-          </Form.Control>
-          <Form.FieldErrors />
-        </Form.Field>
-      </Card.Content>
-      <Card.Footer>
-        <Form.Button>Login</Form.Button>
-      </Card.Footer>
-    </form>
-  </Card.Root>
+  <Tabs.Root value="register" class="w-[400px]">
+    <Tabs.List class="grid w-full grid-cols-2">
+      <Tabs.Trigger value="register">Register</Tabs.Trigger>
+      <Tabs.Trigger value="login">Log In</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Content value="register">
+      <p>todo</p>
+    </Tabs.Content>
+    <Tabs.Content value="login">
+      <Card.Root>
+        <Card.Header>
+          <Card.Title>Log in to tasks tracker</Card.Title>
+          <Card.Description>Enter your credentials.</Card.Description>
+        </Card.Header>
+        <form method="POST" use:enhance>
+          <Card.Content>
+            <Form.Field {form} name="username">
+              <Form.Control let:attrs>
+                <Form.Label>Username</Form.Label>
+                <Input {...attrs} bind:value={$formData.username} />
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+            <Form.Field {form} name="password">
+              <Form.Control let:attrs>
+                <Form.Label>Password</Form.Label>
+                <Input {...attrs} bind:value={$formData.password} />
+              </Form.Control>
+              <Form.FieldErrors />
+            </Form.Field>
+          </Card.Content>
+          <Card.Footer>
+            <Form.Button>Login</Form.Button>
+          </Card.Footer>
+        </form>
+      </Card.Root>
+    </Tabs.Content>
+  </Tabs.Root>
 </main>
