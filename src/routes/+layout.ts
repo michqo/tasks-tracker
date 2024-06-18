fb9ffe89ae2c942aca801241effbf3255d142649
michqo <42279279+michqo@users.noreply.github.com>
2024-06-18 17:55:09 +0200

    feat: move redirect to layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,19 @@
 import { browser } from '$app/environment';
+import { token } from '$lib/utils/stores';
+import { redirect } from '@sveltejs/kit';
 import { QueryClient } from '@tanstack/svelte-query';
+import { get } from 'svelte/store';
 import type { LayoutLoad } from './$types';
 
-export const load: LayoutLoad = () => {
+export const load: LayoutLoad = ({ url }) => {
+  if (browser) {
+    const jwt = get(token);
+    if (jwt == '' && url.pathname != '/auth') {
+      redirect(307, '/auth');
+    } else if (jwt != '' && url.pathname == '/auth') {
+      redirect(308, '/');
+    }
+  }
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
