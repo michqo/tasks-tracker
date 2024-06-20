3c4cde215bd7cc9e70e25ba1ce39dc397fb819f3
michqo <42279279+michqo@users.noreply.github.com>
2024-06-20 17:22:40 +0200

    fix: stage formatted files in husky hook

--- src/routes/tasks/[id]/+page.svelte
+++ src/routes/tasks/[id]/+page.svelte
@@ -8,7 +8,6 @@
   import { api, transformItems } from '$lib/utils/api';
   import { token } from '$lib/utils/stores';
   import { createMutation, createQuery, useQueryClient } from '@tanstack/svelte-query';
-  import { date } from 'zod';
 
   const client = useQueryClient();
   let formOpen = false;
@@ -22,7 +21,7 @@
   const userQuery = createQuery({
     queryKey: ['usersMe'],
     queryFn: () => api().getUsersMe()
-  })
+  });
 
   const postMutation = createMutation({
     mutationFn: (task: string) => {
