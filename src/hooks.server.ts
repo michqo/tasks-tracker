1bc2f4859a9f197ee3a2369c879f9cca78ea5c90
michqo <42279279+michqo@users.noreply.github.com>
2024-06-26 10:44:01 +0200

    feat: move auth checks to hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,15 @@
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  event.locals.accessToken = event.cookies.get('access_token');
+  const accessToken = event.cookies.get('access_token');
+  event.locals.accessToken = accessToken;
+
+  const isAuth = event.url.pathname.startsWith('/auth');
+  if (!accessToken && !isAuth) {
+    redirect(307, '/auth');
+  } else if (accessToken && isAuth) {
+    redirect(308, '/');
+  }
 
   const response = await resolve(event);
   return response;
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,13 +1,6 @@
-import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async ({ route, locals }) => {
-  if (!locals.accessToken && route.id != '/auth') {
-    redirect(307, '/auth');
-  } else if (locals.accessToken && route.id == '/auth') {
-    redirect(308, '/');
-  }
-
+export const load = (async ({ locals }) => {
   return {
     accessToken: locals.accessToken
   };
